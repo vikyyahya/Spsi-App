a30e86808b2aaecdae1e5aa481a66b20c880aa78
vikyyahya <viky@lenna.ai>
2020-07-19 11:43:20 +0700

    login commit

--- app/src/main/java/ai/lenna/spsiapp/login/LoginActivity.java
+++ app/src/main/java/ai/lenna/spsiapp/login/LoginActivity.java
@@ -2,15 +2,29 @@ package ai.lenna.spsiapp.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import ai.lenna.spsiapp.R;
+import ai.lenna.spsiapp.register.RegisterActivity;
 
 public class LoginActivity extends AppCompatActivity {
+    TextView register;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        register = findViewById(R.id.text_view_register);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/ai/lenna/spsiapp/register/RegisterActivity.java
@@ -0,0 +1,16 @@
+package ai.lenna.spsiapp.register;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import ai.lenna.spsiapp.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
